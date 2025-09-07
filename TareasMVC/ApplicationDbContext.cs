b5ef4e1adacc8f4ea13dc8636a8c0499cba0b0ca
Daniel <barreraquezadadanielalejandro@gmail.com>
2025-09-07 17:29:09 -0600

    Entidades creadas

--- TareasMVC/ApplicationDbContext.cs
+++ TareasMVC/ApplicationDbContext.cs
@@ -10,5 +10,7 @@ namespace TareasMVC
         }
 
         public DbSet<Tarea> Tareas { get; set; }
+        public DbSet<Paso> Pasos { get; set; }
+        public DbSet<ArchivoAdjunto> ArchivosAdjuntos { get; set; }
     }
 }
